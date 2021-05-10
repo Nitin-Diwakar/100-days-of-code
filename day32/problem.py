b667c35db9a3a220b3470b31a93fc5bcbe33b5f6
Nitin Diwakar <diwakarnitin509@gmail.com>
2021-05-10 18:21:30 +0530

    Day 32

---
+++ day32/problem.py
@@ -0,0 +1,13 @@
+ 
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+d=[]
+d.append(a)
+d.append(b)
+d.append(c)
+for i in range(0,3):
+    for j in range(0,3):
+        for k in range(0,3):
+            if(i!=j&j!=k&k!=i):
+                print(d[i],d[j],d[k])
