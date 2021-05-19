75d02e024161ce9f4827772f570224c67e56e071
Nitin Diwakar <diwakarnitin509@gmail.com>
2021-05-19 20:35:12 +0530

    Day 41

---
+++ day41/problem.py
@@ -0,0 +1,22 @@
+"""Write a Python program to sort a list of elements using the insertion sort algorithm
+Note : According to Wikipedia "Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time. It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort."
+Sample Data: [14,46,43,27,57,41,45,21,70]
+Expected Result : [14, 21, 27, 41, 43, 45, 46, 57, 70]"""
+
+
+
+def insertionSort(nlist):
+   for index in range(1,len(nlist)):
+
+     currentvalue = nlist[index]
+     position = index
+
+     while position>0 and nlist[position-1]>currentvalue:
+         nlist[position]=nlist[position-1]
+         position = position-1
+
+     nlist[position]=currentvalue
+
+nlist = [14,46,43,27,57,41,45,21,70]
+insertionSort(nlist)
+print(nlist)
