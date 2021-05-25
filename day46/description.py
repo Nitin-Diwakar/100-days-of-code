8a829193eba4570a81f1f9138c8ea733d70eb2c9
Nitin Diwakar <diwakarnitin509@gmail.com>
2021-05-25 19:30:16 +0530

    Day 46

---
+++ day46/description.py
@@ -0,0 +1,8 @@
+def pascal_triangle(n):
+   trow = [1]
+   y = [0]
+   for x in range(max(n,0)):
+      print(trow)
+      trow=[l+r for l,r in zip(trow+y, y+trow)]
+   return n>=1
+pascal_triangle(6) 
