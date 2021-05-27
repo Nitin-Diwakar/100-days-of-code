0aeac17d8b93313e1f414bfec0cd811b9d9498ef
Nitin Diwakar <diwakarnitin509@gmail.com>
2021-05-27 17:19:13 +0530

    Day 48

---
+++ day48/problem.py
@@ -0,0 +1,13 @@
+import json
+# a Python object (dict):
+python_obj = {
+  "name": "David",
+  "class":"I",
+  "age": 6  
+}
+print(type(python_obj))
+# convert into JSON:
+j_data = json.dumps(python_obj)
+
+# result is a JSON string:
+print(j_data)
